fc09f8866835bcac26dc44cf3e0a20bfaee3739a
martyshka200612 <144005569+martyshka200612@users.noreply.github.com>
2024-03-05 17:28:34 +0300

    laba1asm.cpp

--- laba1asm.cpp
+++ laba1asm.cpp
@@ -1,33 +1,35 @@
 #include <iostream>
+
 int main() {
+    short a1, a2, a3;
+    short b1, b2, b3;
+    short c1, c2, c3;
+    long long result;
 
+    std::cin >> a1 >> a2 >> a3;
+    std::cin >> b1 >> b2 >> b3;
 
-	__int16 a1, a2, a3;
-	__int16 b1, b2, b3;
-	__int16 c1, c2, c3;
-	__int64 result;
+    __asm {
+        mov ax, a1
+        add ax, b1
+        mov c1, ax
 
-	std::cin >> a1 >> a2 >> a3;
-	std::cin >> b1 >> b2 >> b3;
-	_asm {
-		mov ax, a1
-		add ax, b1
-		mov c1, ax
+        mov ax, a2
+        adc ax, b2
+        mov c2, ax
 
-		mov ax, a2
-		adc ax, b2
-		mov c2, ax
+        mov ax, a3
+        adc ax, b3
+        mov c3, ax
+    }
 
-		mov ax, a3
-		adc ax, b3
-		mov c3, ax
-	}
-	result = ((static_cast<__int64>(c3)) << 32) + ((static_cast<__int64>(c2)) << 16) + static_cast<__int64>(c1);
+    result = ((static_cast<long long>(c3)) << 32) + ((static_cast<long long>(c2)) << 16) + static_cast<long long>(c1);
 
-	std::cout << "10 c/c: " << '\n';
-	std::cout << result << '\n';
+    std::cout << "10 c/c: " << '\n';
+    std::cout << result << '\n';
 
-	std::cout << "16 c/c: " << '\n';
-	std::cout << std::hex << result << '\n';
+    std::cout << "16 c/c: " << '\n';
+    std::cout << std::hex << result << '\n';
 
+    return 0;
 }
